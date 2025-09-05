26b2c428873edcec2c41de85eb65ce94881c2293
Jose Alejandro <214848445+JoseRodriguez0001@users.noreply.github.com>
2025-09-05 13:50:10 -0500

    interfaz de servicios de driver license

--- taller01/src/main/java/edu/unimag/taller01/repositories/DriverLicenseRepository.java
+++ taller01/src/main/java/edu/unimag/taller01/repositories/DriverLicenseRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DriverLicenseRepository  extends JpaRepository<DriverLicense, Long>, JpaSpecificationExecutor<DriverLicense> {
     Optional<DriverLicense> findByLicenseNumber( String licenseNumber);
+    List<DriverLicense> id(Long id);
     //Solo se implementan consultas complejas
 }
